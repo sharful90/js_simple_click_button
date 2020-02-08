3539674cd074afd5d979df81d6f16edc02bbe5a4
Your Name <bdlifehacks32@gmail.com>
2020-02-08 10:30:20 +0600

    2/8/2020

---
+++ script.js
@@ -0,0 +1,23 @@
+
+
+document.getElementById("btn1").addEventListener("click",function(){
+
+    document.getElementById("text").setAttribute("style","opacity:1;height:160px;");
+
+});
+document.getElementById("btn2").addEventListener("click",function(){
+
+    document.getElementById("text").setAttribute("style","opacity:0;transition:1s;height:0px;");
+
+});
+
+
+
+
+
+
+
+
+
+
+
